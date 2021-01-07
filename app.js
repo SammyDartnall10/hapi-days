5919ec14cadeb319d20632b60d2f39f611bb39ec
Sammy <samantha.megan10@gmail.com>
2021-01-06 19:50:51 -0700

    testing out routes

--- app.js
+++ app.js
@@ -1,17 +1,53 @@
 const Hapi = require('hapi');
 
+// const user = "Sammy"
+
 const init = async () => {
 
   const server = Hapi.server({
     port: 3000,
     host: 'localhost'
   });
-
+  // path is the route 
+  //handler = business logic of the route and sets the response
   server.route({
     method: 'GET',
-    path: '/hello',
+    path: '/hello/{user}',
     handler: (request, h) => {
-      return 'Hello World!'
+      return `Hello ${request.params.user}`
+    }
+  })
+
+  // route with an optional parameter
+  // if you dont add a parameter comes through/returns undefined
+  server.route({
+    method: 'GET',
+    path: '/optionalparams/{optional?}',
+    handler: function (request, h) {
+      return `Optional parameter is ${request.params.optional}`;
+    }
+  })
+
+  //route with partial params coverage 
+  // logging request and h just for interest
+  server.route({
+    method: 'GET',
+    path: '/partials',
+    handler: (request, h) => {
+      console.log(h)
+      console.log("--------------------------")
+      console.log(request)
+      return "hello"
+    }
+  })
+
+  // query parameters
+  // query is an object in the response object, so use dot notation too access each
+  server.route({
+    method: 'GET',
+    path: '/testingquery',
+    handler: function (request, h) {
+      return `Hello ${request.query.name}`
     }
   })
 
